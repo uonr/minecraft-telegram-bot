c9d3b9ca8e240d80ed30e27a03edcc974811079b
uonr <dev@yuru.me>
2025-03-25 06:24:48 +0000

    chore: do not log

--- bot.py
+++ bot.py
@@ -108,7 +108,7 @@ async def edit_group_name(context: ContextTypes.DEFAULT_TYPE):
                 online_count = matched.group(0)
                 count["local"] = int(online_count)
         except Exception as e:
-            logger.warning("Error on getting online count", e)
+            pass
         online_count_from_other_server = await remote_online_count()
         if online_count_from_other_server is not None:
             count['remote'] = online_count_from_other_server
